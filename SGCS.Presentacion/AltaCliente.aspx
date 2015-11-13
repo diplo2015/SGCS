3eb701bdb7aa87abe549a9cad8b3708fe18298be
diplo2015 <yoangeles@hotmail.com>
2015-11-13 20:52:44 -0300

    Alta de cliente

--- SGCS.Presentacion/AltaCliente.aspx
+++ SGCS.Presentacion/AltaCliente.aspx
@@ -9,7 +9,38 @@
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
+    <div style="height: 363px">
+    
+        ALTA DE CLIENTES<br />
+        <br />
+&nbsp;&nbsp; DNI:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+&nbsp;&nbsp; Nombre:&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server" Width="285px"></asp:TextBox>
+        <br />
+        <br />
+&nbsp;&nbsp; Apellido:&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server" Width="282px"></asp:TextBox>
+        <br />
+        <br />
+&nbsp;&nbsp; Dirección:
+        <asp:TextBox ID="TextBox4" runat="server" Width="280px"></asp:TextBox>
+        <br />
+        <br />
+&nbsp;&nbsp; Teléfono:&nbsp;
+        <asp:TextBox ID="TextBox5" runat="server" Width="278px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" Text="Aceptar" Width="133px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Text="Cancelar" Width="136px" />
+        <br />
+        <br />
     
     </div>
     </form>
--- SGCS.Presentacion/Baja Cliente.aspx.designer.cs
+++ SGCS.Presentacion/Baja Cliente.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace SGCS.Web {
     
     
-    public partial class AltaCliente {
+    public partial class Baja_Cliente {
         
         /// <summary>
         /// Control form1.
@@ -29,42 +29,6 @@ namespace SGCS.Web {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
-        
-        /// <summary>
-        /// Control TextBox2.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
-        
-        /// <summary>
-        /// Control TextBox3.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox3;
-        
-        /// <summary>
-        /// Control TextBox4.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox4;
-        
-        /// <summary>
-        /// Control TextBox5.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox5;
         
         /// <summary>
         /// Control Button1.
